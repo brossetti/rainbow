2c4a1da55d8a89030fff55b47f16ae70e69b972e
Blair Rossetti <blair@rossetti.codes>
2022-07-14 14:17:30 -0600

    works on single layer but callbacks don't trigger with multiple

--- src/rainbow/_inspect.py
+++ src/rainbow/_inspect.py
@@ -1,15 +1,14 @@
+import _utils
+
 import numpy as np
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qtagg import FigureCanvas
-# from matplotlib.backends.backend_qtagg import (
-#     NavigationToolbar2QT as NavigationToolbar,
-# )
-# from matplotlib.backends.qt_compat import QtWidgets
-
+from matplotlib.backends.backend_qtagg import (
+    NavigationToolbar2QT as NavigationToolbar,
+)
 # from qtpy.QtGui import QPixmap
 from qtpy.QtWidgets import (
-    QMainWindow,
     QWidget,
     QVBoxLayout,
     QLabel,
@@ -19,102 +18,146 @@ from qtpy.QtWidgets import (
     QGroupBox,
     QHBoxLayout,
     QLineEdit,
-    QPushButton,
-    QDoubleSpinBox
+    QPushButton
 )
 
-# class InspectionWidget(QMainWindow):
+
 class InspectionWidget(QWidget):
     def __init__(self, napari_viewer):
         super().__init__()
         self.viewer = napari_viewer
 
-        # add mouse move callback to all selected layers
+        # initialize canvas
+        self._canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        self._axes = self._canvas.figure.subplots()
+        self.toolbar = NavigationToolbar(self._canvas, self)
+
+        # create controls
+        label_normalization = QLabel('normalization:')
+        cbox_normalization = QComboBox()
+        cbox_normalization.addItems(['max', 'sum', 'raw'])
+        cbox_normalization.activated.connect(self._normalization_changed)
+
+        # create layout
+        layout_settings = QHBoxLayout()
+        layout_settings.addWidget(label_normalization)
+        layout_settings.addWidget(cbox_normalization)
+        layout_main = QVBoxLayout()
+        layout_main.addWidget(self._canvas)
+        layout_main.addWidget(self.toolbar)
+        layout_main.addLayout(layout_settings)
+        self.setLayout(layout_main)
+
+        # define default plot settings
+        self._settings = {
+            'normalization': 'max',
+            'upper_bound_normed': 1.05,
+            }
+
+        self._layer_properties = {}
+
+        # set up callbacks and plot settings for current selections
+        self._layer_selection_changed()
+        self.viewer.layers.selection.events.changed.connect(self._layer_selection_changed)
+
+
+    def _layer_selection_changed(self, event=None):
         for layer in self.viewer.layers:
+            # ensure layer is an image of appropriate dimensions
             if layer._type_string == 'image' and layer.ndim > self.viewer.dims.ndisplay:
-                if self._plot_spectra not in layer.mouse_move_callbacks:
-                    layer.mouse_move_callbacks.append(self._plot_spectra)
+                layer_id = id(layer)
 
+                if layer in self.viewer.layers.selection:
+                    # add mouse move callback to all selected layers
+                    if self._mouse_moved not in layer.mouse_move_callbacks:
+                        layer.mouse_move_callbacks.append(self._mouse_moved)
+                    
+                        # determine effective bit depth
+                        effective_bit_depth = _utils.effective_bit_depth(layer.data)
 
-        # initialize interface
-        self.view = FigureCanvas(Figure(figsize=(5, 3)))
-        self.axes = self.view.figure.subplots()
-        # self.toolbar = NavigationToolbar2QT(self.view, self)
-        self.mu_input = QDoubleSpinBox()
-        self.std_input = QDoubleSpinBox()
-        self.mu_input.setPrefix("μ: ")
-        self.std_input.setPrefix("σ: ")
-        self.std_input.setValue(10)
+                        # determine number of spectral channels
+                        nchannels = layer.data.shape[0]
 
-        #  Create layout
-        input_layout = QHBoxLayout()
-        input_layout.addWidget(self.mu_input)
-        input_layout.addWidget(self.std_input)
-        vlayout = QVBoxLayout()
-        # vlayout.addWidget(self.toolbar)
-        vlayout.addWidget(self.view)
-        vlayout.addLayout(input_layout)
-        self.setLayout(vlayout)
+                        # set null spectrum
+                        spectrum = np.zeros(nchannels)
 
-        # connect inputs with on_change method
-        # self.mu_input.valueChanged.connect(self.on_change)
-        # self.std_input.valueChanged.connect(self.on_change)
+                        # plot null spectrum
+                        line = self._axes.plot(spectrum)
 
-        # self.on_change()
+                        # store layer properties for later
+                        self._layer_properties[layer_id] = {
+                            'effective_bit_depth': effective_bit_depth,
+                            'nchannels': nchannels,
+                            'spectrum': spectrum,
+                            'line': line
+                            }
+  
+                elif self._mouse_moved in layer.mouse_move_callbacks:
+                    # remove mouse move callback if layer has been unselected
+                    layer.mouse_move_callbacks.remove(self._mouse_moved)
 
-        
-        # self._main = QWidget()
-        # self.setCentralWidget(self._main)
+                    # remove line from plot
+                    self._axes.lines.remove(self._layer_properties[layer_id]['line'][0])
+                    
+                    # remove layer from properties dictionary
+                    del self._layer_properties[layer_id]
+            
+        # define upper bound for raw data
+        max_bit_depth = np.max([self._layer_properties[id]['effective_bit_depth'] for id in self._layer_properties])
+        max_nchannels = np.max([self._layer_properties[id]['nchannels'] for id in self._layer_properties])
 
-        # layout = QVBoxLayout(self._main)
-        # dynamic_canvas = FigureCanvas(Figure(figsize=(5, 3)))
-        # layout.addWidget(dynamic_canvas)
-        # layout.addWidget(NavigationToolbar(dynamic_canvas, self))
-        # self._dynamic_ax = dynamic_canvas.figure.subplots()
-        # Set up a Line2D.
-        # (self._line,) = self._dynamic_ax.plot(layer.data[:,0,0])
-
-        # layout = QVBoxLayout(self._main)
-
-        # # create canvas element for plotting
-        # canvas = FigureCanvas(Figure(figsize=(5, 3)))
-        # self._axes = canvas.figure.subplots()
-
-        # # create settings elements
-        self._normalization = 0
-        # # label_normalization = QLabel('normalization:')
-        # # setting_normalization = QComboBox()
-        # # setting_normalization.addItems(['max', 'sum', 'raw'])
-        # # setting_normalization.activated.connect(self.normalization_changed)
-
-        # # add everything to the main vbox layout
-        # layout.addWidget(canvas)
-        # layout.addWidget(NavigationToolbar(canvas, self))
-        # # layout.addWidget(label_normalization)
-        # # layout.addWidget(setting_normalization)
-
-        # # Set up a Line2D.        
-        # (self._line,) = self._axes.plot(layer.data[:,0,0])
+        self._settings.update({
+            'upper_bound_raw': 2**max_bit_depth * 1.05,
+            'nchannels': max_nchannels
+            })
 
     
-    def normalization_changed(self, ind):
-        self._normalization = ind
+    def _normalization_changed(self, ind):
+        if ind == 0:
+            self._settings['normalization'] = 'max'
+            self._axes.set_ybound(upper=self._settings['upper_bound_normed'])
+        elif ind == 1:
+            self._settings['normalization'] = 'sum'
+            self._axes.set_ybound(upper=self._settings['upper_bound_normed'])
+        else:
+            self._settings['normalization'] = 'raw'
+            self._axes.set_ybound(upper=self._settings['upper_bound_raw'])
+        
+        for layer_id in self._layer_properties:
+            self._plot_layer_spectrum(layer_id)
 
-    def _plot_spectra(self, layer, event):
+
+    def _mouse_moved(self, layer, event):
         coordinates = layer.world_to_data(self.viewer.cursor.position)
         xy_coordinates = [int(x) for x in coordinates[-2:]]
         if all(xy_coordinates >= layer.corner_pixels[0,-2:]) and all(xy_coordinates < layer.corner_pixels[1,-2:]):
-            spectrum = layer.data[:,xy_coordinates[0], xy_coordinates[1]]
-            if self._normalization == 0:
-                spectrum = np.divide(spectrum, np.max(spectrum))
-            elif self._normalization == 1:
-                spectrum = np.divide(spectrum, np.sum(spectrum))
-            self._line.set_data(np.linspace(0,22,23),spectrum)
-            self._line.figure.canvas.draw()
+            layer_id = id(layer)
+            self._layer_properties[layer_id]['spectrum'] = layer.data[:,xy_coordinates[0], xy_coordinates[1]]
+            self._plot_layer_spectrum(layer_id)
+
+
+    def _plot_layer_spectrum(self, layer_id):
+        spectrum = self._layer_properties[layer_id]['spectrum']
+
+        if self._settings['normalization'] == 'max':
+            spectrum = _utils.safe_normalize_max(spectrum)
+            self._axes.set_ybound(upper=1.05)
+        elif self._settings['normalization'] == 'sum':
+            spectrum = _utils.safe_normalize_sum(spectrum)
+            self._axes.set_ybound(upper=1.05)
+            
+        channels = self._layer_properties[layer_id]['nchannels']
+        line = self._layer_properties[layer_id]['line']
+        line[0].set_data(range(channels), spectrum)
+
+        line[0].figure.canvas.draw()
+
 
 if __name__ == "__main__":
     import napari
     viewer = napari.Viewer()
-    im = napari.utils.io.magic_imread('/Users/brossetti/Desktop/Lepto_10fluors_s001.tif')
-    viewer.add_image(im)
+    im1 = napari.utils.io.magic_imread('/Users/brossetti/Desktop/Lepto_10fluors_s001.tif')
+    im2 = im1.copy() - 1
+    viewer.add_image(im1)
+    viewer.add_image(im2)
     napari.run()
